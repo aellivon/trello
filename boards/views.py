065f84b8a1cf6c1778c64bf54bbed78d83dfdf03
aellivon <aellivon@gmail.com>
2018-07-03 09:22:01 +0800

    adding a persisten modal and fixing some validation errors

--- boards/views.py
+++ boards/views.py
@@ -14,19 +14,25 @@ class IndexView(TemplateView):
     """
     template_name = "boards/index.html"
     form = BoardModalForm
+
+    def show_boards(self, user):
+        boards = BoardMember.objects.filter(user=user,board__archived=False)
+        # This code allows the template to count the current board
+        count = 0
+        for board in boards:
+            board.count = count
+            count+=1
+            if count == 5:
+                count = 0
+        return boards
+
+
     def get(self, *args,** kwargs):
         if self.request.user.is_authenticated:
             context = self.form()
             username = self.kwargs.get('username')
             user = get_object_or_404(User,username=username)
-            boards = BoardMember.objects.filter(user=user)
-            # This code allows the template to count the current board
-            count = 0
-            for board in boards:
-                board.count = count
-                count+=1
-                if count == 5:
-                    count = 0
+            boards = self.show_boards(user)
             return render(self.request, self.template_name, {'form':context, 'boards': boards})
         else:
             return HttpResponseRedirect(reverse('users:log_in'))
@@ -34,21 +40,20 @@ class IndexView(TemplateView):
     def post(self, *args,** kwargs):
         if self.request.user.is_authenticated:
             form = self.form(self.request.POST)
+            username = self.request.user.get_username()
+            user = get_object_or_404(User,username=username)
             if form.is_valid():
-                username = self.request.user.get_username()
-                user = get_object_or_404(User,username=username)
                 form.save(user)
-                boards = BoardMember.objects.filter(user=user,archived=False)
-                # This code allows the template to count the current board
-                count = 0
-                for board in boards:
-                    board.count = count
-                    count+=1
-                    if count == 5:
-                        count = 0
+                boards = self.show_boards(user)
+                return render(self.request, self.template_name, {'form':form, 'boards': boards})
+            else:
+                boards = self.show_boards(user)
                 return render(self.request, self.template_name, {'form':form, 'boards': boards})
         else:
             return HttpResponseRedirect(reverse('users:log_in'))
+
+
+    
 
 class BoardView(TemplateView):
     template_name = "boards/boards.html"
